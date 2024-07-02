0d4eb129025b298864b9e528c310c15ffb2a2204
Oleg <antoshinov@gmail.com>
2024-07-02 15:38:45 +1000

    add new db

--- database.sql
+++ database.sql
@@ -1,6 +1,16 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 CREATE TABLE urls (
   id bigint PRIMARY KEY,
   url VARCHAR(255),
+  created_at DATE
+);
+
+CREATE TABLE url_checks (
+  id INT PRIMARY KEY,
+  url_id INT,
+  status_code INT,
+  h1 VARCHAR(255),
+  description TEXT,
   created_at DATE
 );
