72bc379c5f74e66cf8a47a06a229049ab68b86cf
Oleg <antoshinov@gmail.com>
2024-07-05 19:22:44 +1000

    auto loading of env

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+
+ENV_FILE="./.env"
+
+if [ -f "$ENV_FILE" ]; then
+    source $ENV_FILE
+else
+    echo "Файл с переменными окружения не найден, идёт загрузка из хостинга"
+fi
+
+
 make install && psql -a -d $DATABASE_URL -f database.sql
